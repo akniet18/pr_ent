4cb332e1fd18d2cd7d69efe00219509fb5419610
akniet18 <akniet.akniet2016@mail.ru>
2020-10-20 13:36:08 +0600

    first

--- ent/settings.py
+++ ent/settings.py
@@ -154,15 +154,12 @@ REST_FRAMEWORK = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-
-EMAIL_HOST_USER = 'bilimcenter20@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bilim20centre20'
-
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bilimcenter20@gmail.com'
+EMAIL_HOST_PASSWORD = 'Bilim20centre20'
+EMAIL_PORT = 587
 
 
 # Static files (CSS, JavaScript, Images)
