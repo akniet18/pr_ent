00021a1d51220f8d2e2a34841aade8908172dd62
akniet18 <akniet.akniet2016@mail.ru>
2020-11-20 23:11:24 +0600

    first

--- ent/settings.py
+++ ent/settings.py
@@ -157,7 +157,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'Bilimcentre@gmail.com'
+EMAIL_HOST_USER = 'bilimcentre@gmail.com'
 EMAIL_HOST_PASSWORD = 'Bilim20centre20'
 EMAIL_PORT = 587
 
