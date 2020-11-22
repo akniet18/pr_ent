6daf9d32fd76ac50ad07759e604a8291e960e560
akniet18 <akniet.akniet2016@mail.ru>
2020-11-22 22:33:45 +0600

    first

--- entapp/views.py
+++ entapp/views.py
@@ -98,7 +98,7 @@ class Answers(APIView):
 
     def get(self, request, id):
         subject = Subject.objects.get(id = id)
-        questions = subject.test_subject.all()
+        questions = list(subject.test_subject.all())
         random.shuffle(questions)
         questions = questions[:30]
         s = TestSer2(questions, many=True,context={'request': request})
